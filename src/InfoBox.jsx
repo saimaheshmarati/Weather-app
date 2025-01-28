ca25520a24368dceb941784fe8dc8b8ca53e7a3e
saimaheshmarati <msaimahesh26@gmail.com>
2025-01-28 23:52:46 +0530

    Initial commit

--- src/InfoBox.jsx
+++ src/InfoBox.jsx
@@ -8,11 +8,10 @@ import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
 import "./infobox.css";
 
 export default function InfoBox({ info }) {
-    const INIT_URL =
-        "https://media.istockphoto.com/id/1137759901/photo/summer-hot-weather-season-high-temperature-thermometer-with-city-view.webp?a=1&b=1&s=612x612&w=0&k=20&c=mTWF-uHN6hawvdK0rvqiWAYt5Y8E7u_i3N0XTlRRo_Q=";
-    const HOT_URL="https://images.unsplash.com/photo-1504370805625-d32c54b16100?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    const COLD_URL="https://images.unsplash.com/photo-1621124266180-727515a3974a?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    const RAIN_URL="https://images.unsplash.com/photo-1438449805896-28a666819a20?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    const INIT_URL = "./images/init.jpg";
+    const HOT_URL="./images/hot.avif"
+    const COLD_URL="./images/cold.avif"
+    const RAIN_URL="./images/rain.avif"
     return (
         <div className="InfoBox">
             <div className="card">
