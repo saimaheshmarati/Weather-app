ba23ffa4519c5cfcbbc5c197ffd79be59d1b5cde
saimaheshmarati <msaimahesh26@gmail.com>
2025-01-29 18:36:11 +0530

    changes

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,5 +1,5 @@
 import SearchBox from './SearchBox'
-import InfoBox from './infoBox'
+import InfoBox from './InfoBox.jsx'
 import { useState } from 'react'
 export default function WeatherApp() {
     const [weatherInfo, setWeatherInfo] = useState(
